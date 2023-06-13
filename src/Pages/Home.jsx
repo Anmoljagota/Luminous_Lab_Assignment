8a500f5c72d3b1f13e61842f9e7a470f5ae08ac2
Anmoljagota <anmoljagota08@gmail.com>
2023-06-13 10:00:40 +0530

    logo done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,15 @@
 import { Box, Flex, Text } from "@chakra-ui/react";
 import React from "react";
-
+import { FaUserCircle } from "react-icons/fa";
 const Home = () => {
   return (
     <div className="min-h-[65vh] w-8/12 shadow-2xl m-auto bg-white">
       <Box className="m-auto w-[90%]">
-        <Flex>
-          <Text>PhoneBook</Text>
+        <Flex className="border border-black border-solid">
+          <Flex className="border border-red-500 border-solid text-base justify-center	items-center" gap="10px">
+            <FaUserCircle style={{ color: "#1d4ed8", fontSize: "30px" }} />
+            <Text className="text-lg text-slate-600	">PhoneBook</Text>
+          </Flex>
         </Flex>
       </Box>
     </div>
