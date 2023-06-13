9c1db60afcffdfec4bfca4a2e606935da191c378
Anmoljagota <anmoljagota08@gmail.com>
2023-06-13 12:01:07 +0530

    Navbar Done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,21 +1,27 @@
 import { Box, Flex, Text } from "@chakra-ui/react";
 import React from "react";
 import { FaUserCircle } from "react-icons/fa";
+import {BsPlusLg} from "react-icons/bs"
 const Home = () => {
   return (
     <div className="min-h-[65vh] w-8/12 shadow-2xl m-auto bg-white">
       <Box className="m-auto w-[90%]">
-        <Flex className="border border-black border-solid justify-between		items-center">
+        <Flex className="justify-between items-center">
           <Flex className="justify-center	items-center" gap="10px">
             <FaUserCircle style={{ color: "#1d4ed8", fontSize: "30px" }} />
             <Text className="text-lg text-slate-600">PhoneBook</Text>
           </Flex>
           <input
             type="text"
-            className="rounded-md py-1.5 px-2"
-            placeholder="Search.."
+            className="rounded-md py-2 px-6"
+            placeholder="Search..."
             style={{ boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px" }}
           />
+          <Flex>
+            <button className="bg-white py-3 px-5 gap-3	 rounded-lg flex justify-center	items-center font-semibold	"  style={{boxShadow:"rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px"}}>
+             <BsPlusLg/> Create contant
+            </button>
+          </Flex>
         </Flex>
       </Box>
     </div>
