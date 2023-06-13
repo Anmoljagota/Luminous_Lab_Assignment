355755782cb7d690f052bf217d9a7b71fa45a5f3
Anmoljagota <anmoljagota08@gmail.com>
2023-06-13 11:06:13 +0530

    search done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,11 +5,17 @@ const Home = () => {
   return (
     <div className="min-h-[65vh] w-8/12 shadow-2xl m-auto bg-white">
       <Box className="m-auto w-[90%]">
-        <Flex className="border border-black border-solid">
-          <Flex className="border border-red-500 border-solid text-base justify-center	items-center" gap="10px">
+        <Flex className="border border-black border-solid justify-between		items-center">
+          <Flex className="justify-center	items-center" gap="10px">
             <FaUserCircle style={{ color: "#1d4ed8", fontSize: "30px" }} />
-            <Text className="text-lg text-slate-600	">PhoneBook</Text>
+            <Text className="text-lg text-slate-600">PhoneBook</Text>
           </Flex>
+          <input
+            type="text"
+            className="rounded-md py-1.5 px-2"
+            placeholder="Search.."
+            style={{ boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px" }}
+          />
         </Flex>
       </Box>
     </div>
